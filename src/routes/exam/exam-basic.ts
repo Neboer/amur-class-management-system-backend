afba291d6e59f9eb06dbf021c5b50b39cc05afa1
Neboer <rubinposter@gmail.com>
2025-02-14 11:19:24 +0800

    修正了ExamBasic的格式问题

--- src/routes/exam/exam-basic.ts
+++ src/routes/exam/exam-basic.ts
@@ -1,7 +1,7 @@
-import { FastifyPluginCallback } from 'fastify';
-import { require_admin } from '../session/require-privilege';
-import { Type } from '@sinclair/typebox';
-import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+import {FastifyPluginCallback} from 'fastify';
+import {require_admin} from '../session/require-privilege';
+import {Type} from '@sinclair/typebox';
+import {TypeBoxTypeProvider} from '@fastify/type-provider-typebox';
 
 const exam_api: FastifyPluginCallback = (f, opts, done) => {
     const fastify = f.withTypeProvider<TypeBoxTypeProvider>();
@@ -15,7 +15,7 @@ const exam_api: FastifyPluginCallback = (f, opts, done) => {
         const exams = await examModule.get_all_exams();
         return Promise.all(exams.map(async (exam) => {
             const subjects = await examModule.get_subjects_of_exam(exam.id);
-            return { ...exam, subjects };
+            return {...exam, subjects};
         }));
     });
 
@@ -32,10 +32,10 @@ const exam_api: FastifyPluginCallback = (f, opts, done) => {
         fastify.log.info(`Getting exam ${exam_id}`);
         const exam = await examModule.get_exam_by_id(exam_id);
         if (!exam) {
-            return reply.status(404).send({ error: 'Exam not found' });
+            return reply.status(404).send({error: 'Exam not found'});
         }
         const subjects = await examModule.get_subjects_of_exam(exam.id);
-        return { ...exam, subjects };
+        return {...exam, subjects};
     });
 
     // 创建考试
@@ -44,7 +44,7 @@ const exam_api: FastifyPluginCallback = (f, opts, done) => {
         schema: {
             body: Type.Object({
                 name: Type.String(),
-                date: Type.String({ format: 'date' }),
+                date: Type.String({format: 'date'}),
                 subjects: Type.Array(Type.Object({
                     subject: Type.String(),
                     full_score: Type.Number(),
@@ -52,10 +52,10 @@ const exam_api: FastifyPluginCallback = (f, opts, done) => {
             }),
         },
     }, async (request, reply) => {
-        const { name, date, subjects } = request.body;
+        const {name, date, subjects} = request.body;
         fastify.log.info(`Creating exam ${name}`);
-            await examModule.create_exam({ name, date }, subjects);
-            return reply.status(201).send({ status: 'ok' });
+        await examModule.create_exam({name, date}, subjects);
+        return reply.status(201).send({status: 'ok'});
     });
 
     // 更新考试信息（名称、日期）
@@ -67,22 +67,21 @@ const exam_api: FastifyPluginCallback = (f, opts, done) => {
             }),
             body: Type.Object({
                 name: Type.String(),
-                date: Type.String({ format: 'date' }),
+                date: Type.String({format: 'date'}),
             }),
         },
     }, async (request, reply) => {
         const exam_id = Number.parseInt(request.params.exam_id);
-        const { name, date } = request.body;
+        const {name, date} = request.body;
         fastify.log.info(`Updating exam ${exam_id} with ${JSON.stringify(request.body)}`);
         const exam = await examModule.get_exam_by_id(exam_id);
         if (!exam) {
-            return reply.status(404).send({ error: 'Exam not found' });
+            return reply.status(404).send({error: 'Exam not found'});
         }
         exam.name = name;
         exam.date = date;
-            await examModule.update_exam(exam);
-            return { status: 'ok' };
-
+        await examModule.update_exam(exam);
+        return {status: 'ok'};
     });
 
     // 删除考试
@@ -98,10 +97,10 @@ const exam_api: FastifyPluginCallback = (f, opts, done) => {
         fastify.log.info(`Deleting exam ${exam_id}`);
         const exam = await examModule.get_exam_by_id(exam_id);
         if (!exam) {
-            return reply.status(404).send({ error: 'Exam not found' });
+            return reply.status(404).send({error: 'Exam not found'});
         }
-            await examModule.delete_exam(exam_id);
-            return { status: 'ok' };
+        await examModule.delete_exam(exam_id);
+        return {status: 'ok'};
     });
 
     done();
